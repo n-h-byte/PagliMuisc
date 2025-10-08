74b1452790aba013791b695adac595d77e0795ba
n-h-byte <hritwiksupercell@gmail.com>
2025-10-08 06:11:46 +0530

    Update __main__.py

--- AviaxMusic/__main__.py
+++ AviaxMusic/__main__.py
@@ -5,11 +5,11 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AviaxMusic import LOGGER, app, userbot
-from AviaxMusic.core.call import Aviax
-from AviaxMusic.misc import sudo
-from AviaxMusic.plugins import ALL_MODULES
-from AviaxMusic.utils.database import get_banned_users, get_gbanned
+from NapsterMusic import LOGGER, app, userbot
+from NapsterMusic.core.call import Napster
+from NapsterMusic.misc import sudo
+from NapsterMusic.plugins import ALL_MODULES
+from NapsterMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
 
@@ -35,27 +35,27 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("AviaxMusic.plugins" + all_module)
-    LOGGER("AviaxMusic.plugins").info("Successfully Imported Modules...")
+        importlib.import_module("NapsterMusic.plugins" + all_module)
+    LOGGER("NapsterMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
-    await Aviax.start()
+    await Napster.start()
     try:
-        await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Napster.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("AviaxMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        LOGGER("NapsterMusic").error(
+            "Please turn on the videochat of your log group/channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
-    await Aviax.decorators()
-    LOGGER("AviaxMusic").info(
-        "\x41\x76\x69\x61\x78\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x41\x76\x69\x61\x78\x4f\x66\x66\x69\x63\x69\x61\x6c"
+    await Napster.decorators()
+    LOGGER("NapsterMusic").info(
+        "Napster Music Started Successfully.\n\nDon't forget to visit @NapsterOfficial"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("AviaxMusic").info("Stopping Aviax Music Bot...")
+    LOGGER("NapsterMusic").info("Stopping Napster Music Bot...")
 
 
 if __name__ == "__main__":
