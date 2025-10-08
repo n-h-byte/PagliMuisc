681e0813d4a6475e3e683e4169e30002f339c056
n-h-byte <hritwiksupercell@gmail.com>
2025-10-08 06:34:11 +0530

    Update extraction.py

--- AviaxMusic/utils/extraction.py
+++ AviaxMusic/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from AviaxMusic import app
+from NapsterMusic import app
 
 
 async def extract_user(m: Message) -> User:
@@ -15,3 +15,4 @@ async def extract_user(m: Message) -> User:
         if m.command[1].isdecimal()
         else m.command[1]
     )
+
